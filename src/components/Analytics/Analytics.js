3823e0021377efe07cf5ca613ff1b7fd50414e10
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2024-04-02 00:38:24 -0700

    work progress analytics

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,44 +1,100 @@
-import React, { useState } from "react";
-import { BetaAnalyticsDataClient } from "@google-analytics/data";
-propertyId = '406180211';
-
+import React, { useState, useEffect } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../FirebaseConfig";
+import { Table } from "./Table";
 
 const Analytics = () => {
+  const [authenticated, setAuthenticated] = useState(
+    !!localStorage.getItem("accessToken")
+  );
+  const [errors, setError] = useState("");
+  const [report, setReport] = useState(null);
+  const propertyId = "406180211";
 
+  useEffect(() => {
+    if (authenticated) {
+      runReport();
+    }
+  }, [authenticated]);
 
-async function runReport() {
-    const [response] = await analyticsDataClient.runReport({
-      property: `properties/${propertyId}`,
-      dateRanges: [
-        {
-          startDate: '2020-03-31',
-          endDate: 'today',
-        },
-      ],
-      dimensions: [
-        {
-          name: 'city',
-        },
-      ],
-      metrics: [
-        {
-          name: 'activeUsers',
-        },
-      ],
-    });
-  
-    console.log('Report result:');
-    response.rows.forEach(row => {
-      console.log(row.dimensionValues[0], row.metricValues[0]);
-    });
+  async function signIn() {
+    if (!authenticated) {
+      const provider = new GoogleAuthProvider();
+      provider.addScope("https://www.googleapis.com/auth/analytics.readonly");
+      provider.addScope("https://www.googleapis.com/auth/analytics");
+
+      try {
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
+        const credentials = user.stsTokenManager;
+        localStorage.setItem("refreshToken", credentials.refreshToken);
+        localStorage.setItem("accessToken", credentials.accessToken);
+        localStorage.setItem("name", user.displayName);
+        localStorage.setItem("user", user);
+        setAuthenticated(true);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+        setAuthenticated(false);
+      }
+    } else {
+      setAuthenticated(true);
+    }
   }
 
-  runReport();
-    return (
-        <div>
-            <h1>Analytics</h1>
-        </div>
+  async function runReport() {
+    var options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({
+        dimensions: [
+          {
+            name: "unifiedScreenName",
+          },
+        ],
+        metrics: [
+          {
+            name: "screenPageViews",
+          },
+        ],
+        dateRanges: [
+          {
+            startDate: "2024-01-01",
+            endDate: "2024-04-01",
+          },
+        ],
+      }),
+    };
+    await fetch(
+      `https://analyticsdata.googleapis.com/v1beta/properties/${propertyId}:runReport`,
+      options
     )
-}
+      .then((response) => response.json())
+      .then((data) => setReport(data));
+  }
 
-export default Analytics
+  return (
+    <div>
+      {errors && <p>{errors}</p>}
+      <h1>Analytics</h1>
+      <h1>Welcome Back {localStorage.getItem("name")}</h1>
+      {!authenticated && (
+        <button
+          onClick={async () => {
+            await signIn();
+          }}
+        >
+          Sign In To Google
+        </button>
+      )}
+
+      {report ? <Table reports={report} /> : "Loading..."}
+    </div>
+  );
+};
+
+export default Analytics;
